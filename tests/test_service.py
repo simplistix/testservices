a839bc610091f5dfda072a8df0f440a6f5cbc0f7
Chris Withers <chris@simplistix.co.uk>
2023-04-14 08:34:05 +0100

    Minimal Service API

---
+++ tests/test_service.py
@@ -0,0 +1,48 @@
+from testfixtures import compare
+from testfixtures.mock import Mock, call
+from testservices.service import Service
+
+
+def test_minimal():
+    service = Service()
+    with service as s:
+        assert s is service
+
+
+class MockService(Service):
+
+    def __init__(self, mock: Mock, name: str, *args):
+        self.mock = getattr(mock, name)
+        self.mock.init(*args)
+        super().__init__()
+
+    def start(self):
+        self.mock.start()
+
+    def stop(self):
+        self.mock.stop()
+
+    def cleanup(self):
+        self.mock.cleanup()
+
+    def get(self):
+        self.mock.get()
+        return self
+
+    def use(self):
+        self.mock.use()
+
+
+def test_single():
+    mock = Mock()
+    service = MockService(mock, 'service')
+    with service:
+        service.use()
+    compare(mock.mock_calls, expected=[
+        call.service.init(),
+        call.service.start(),
+        call.service.get(),
+        call.service.use(),
+        call.service.stop(),
+        call.service.cleanup(),
+    ])
