1c7e567b1f820c9c251e8de6cf5fb9e0810e4b15
Chris Withers <chris@simplistix.co.uk>
2023-04-14 08:34:05 +0100

    Minimal Service API

--- testservices/service.py
+++ testservices/service.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Sequence, Type, Optional
 
 from .provider import Provider
 from .supplier import Supplier
@@ -8,3 +8,28 @@ class Service:
     name: str
     providers: List[Provider]
     suppliers: List[Supplier]
+
+    def start(self):
+        pass
+
+    def get(self):
+        return self
+
+    def stop(self):
+        pass
+
+    def cleanup(self):
+        pass
+
+    def __enter__(self):
+        self.start()
+        return self.get()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
+        self.cleanup()
+
+
+class Dependency:
+    service: Type[Service]
+    name: Optional[str]
