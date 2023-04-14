60f30f1ef74910c17658b9f415cb029d9c082609
Chris Withers <chris@simplistix.co.uk>
2023-04-14 08:42:58 +0100

    Drop 3.6 as it has no dataclasses

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     packages=find_packages(exclude=["tests"]),
     zip_safe=False,
     include_package_data=True,
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     extras_require=dict(
         test=[
             'PyMySQL',
